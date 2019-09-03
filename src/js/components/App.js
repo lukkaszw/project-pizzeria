95bfd65fb8235b453f5d7bdf0174f54cf9afe6e8
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2019-09-03 20:55:45 +0200

    Add booking page

--- src/js/components/App.js
+++ src/js/components/App.js
@@ -1,11 +1,52 @@
-import { select } from '../settings.js';
+import { select, classNames } from '../settings.js';
 import Cart from './Cart.js';
 import Product from './Product.js';
+import Booking from './Booking.js';
 
 
 class App {
   constructor(dbPaths) {
     this.dbPaths = dbPaths;
+  }
+
+  initPages() {
+    this.pages = document.querySelector(select.containerOf.pages).children;
+    this.navLinks = document.querySelectorAll(select.nav.links);
+    const pageIdFromHash = window.location.hash.replace('#/', '');
+
+    let pageMatchingHash = this.pages[0].id;
+
+    for(let page of this.pages) {
+      if(page.id === pageIdFromHash) {
+        pageMatchingHash = page.id;
+        break;
+      }
+    }
+
+    this.activatePage(pageMatchingHash);
+
+    for(let link of this.navLinks) {
+      link.addEventListener('click', (e) => {
+        e.preventDefault();
+        const pageId = e.target.getAttribute('href').replace('#', '');
+        this.activatePage(pageId);
+        window.location.hash = '#/' + pageId;
+      });
+    }
+  }
+
+  activatePage(pageId) {
+    for(let page of this.pages) {
+      page.classList.toggle(classNames.pages.active, page.id === pageId);
+    }
+
+    for(let link of this.navLinks) {
+      link.classList.toggle(
+        classNames.pages.active,
+        link.getAttribute('href') === `#${pageId}`
+      );
+    }
+
   }
 
   initData() {
@@ -47,10 +88,17 @@ class App {
     });
   }
 
+  initBooking() {
+    const bookingElement = document.querySelector(select.containerOf.booking);
+    new Booking(bookingElement);
+  }
+
   init() {
+    this.initPages();
     this.initData();
     this.initMenu();
     this.initCart();
+    this.initBooking();
   }
 
 }
