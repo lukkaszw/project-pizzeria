3ad4c1afe6dc63bdb65591515a206e5cabdce59c
Lukasz Wojdat <lukaszwojdat88@gmail.com>
2019-09-15 12:30:38 +0200

    Add  changes to deployment

--- src/js/settings.js
+++ src/js/settings.js
@@ -139,7 +139,7 @@ export const settings = {
     },
   },
   db: {
-    url: 'http://localhost:3131',
+    url: '//' + window.location.hostname + (window.location.hostname=='localhost' ? ':3131' : ''),
     product: 'product',
     order: 'order',
     booking: 'booking',
